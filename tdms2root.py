7b4a16149fc9390892ebf0e148cd10cfe51b9d54
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2020-08-04 13:52:19 +0800

    debug

--- tdms2root.py
+++ tdms2root.py
@@ -7,8 +7,8 @@ import yaml
 import sys
 from array import array
 
-if len(sys.argv) != 3:
-    print ("USAGE: %s <input file> <output file>" %(sys.argv[0]))
+if len(sys.argv) != 2:
+    print ("USAGE: %s <input file>" %(sys.argv[0]))
     sys.exit(1)
 
 tdmsFileName = sys.argv[1]
